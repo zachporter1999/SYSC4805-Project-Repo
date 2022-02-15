d1a6e4d3dbc566ae4ffd5fbc0d4913e3d9d49f73
Xach Porter <zach.ld.porter@gmail.com>
2022-02-15 14:57:59 -0500

    updated python script to use config file. added runRemoteAPI.bat file

--- remoteAPI/remoteApiTest.py
+++ remoteAPI/remoteApiTest.py
@@ -11,11 +11,22 @@ except:
     print ('--------------------------------------------------------------')
     print ('')
 
+import json
 import time
 import sys
 
-coppeliaServerAddress = '127.0.0.1'
-coppeliaServerPort    = 19999
+"""
+    Parses the json config file
+
+    Return:
+        A dictionary containing the configs.
+"""
+def parseConfig(config_filename):
+    f = open(config_filename)
+    config = json.load(f)
+    f.close()
+
+    return config
 
 # ========================================
 # Main
@@ -23,26 +34,74 @@ coppeliaServerPort    = 19999
 if __name__ == '__main__':
     print("Application Starting...")
 
-    sim.simxFinish(-1) # Close all opened connections.
-    clientID = sim.simxStart(coppeliaServerAddress, coppeliaServerPort, True, True, 5000, 5)
+    # --------------------
+    # Load Config
+    # --------------------
 
-    res, leftMotor = sim.simxGetObjectHandle(clientID, "Pioneer_p3dx_leftMotor", sim.simx_opmode_blocking)
+    try:
+        config = parseConfig("config.json")
+    except  FileNotFoundError:
+        # No Config file
+        print("Error: File not found")
+        sys.exit(1)
+    except json.JSONDecodeError:
+        # Json failed to load
+        print("Error: JSON unable to load")
+        sys.exit(2)
+
+    # --------------------
+    # Remote API setup
+    # --------------------
+
+    sim.simxFinish(-1) # Close all opened connections.
+    clientID = sim.simxStart(
+            config["remoteAPI"]["address"],
+            config["remoteAPI"]["port"],
+            True,
+            True, 
+            config["remoteAPI"]["timeout"],
+            5)
+
+    # --------------------
+    # Get Left Motor Handler
+    # --------------------
+
+    res, leftMotor = sim.simxGetObjectHandle(
+            clientID, 
+            config["leftMotor"], 
+            sim.simx_opmode_blocking)
 
     if res != sim.simx_return_ok:
         print("Failed to find left motor")
         sys.exit(-1)
+
+    # --------------------
+    # Get Right Motor Handler
+    # --------------------
     
-    res, rightMotor = sim.simxGetObjectHandle(clientID, "Pioneer_p3dx_rightMotor", sim.simx_opmode_blocking)
+    res, rightMotor = sim.simxGetObjectHandle(
+            clientID, 
+            config["rightMotor"], 
+            sim.simx_opmode_blocking)
 
     if res != sim.simx_return_ok:
         print("Failed to find right motor")
         sys.exit(-1)
 
-    motorSpeed = 10
+    # --------------------
+    # Main Loop
+    # --------------------
 
-    for itr in range(0, 100):
-        sim.simxSetJointTargetVelocity(clientID, leftMotor, motorSpeed, sim.simx_opmode_blocking)
-        sim.simxSetJointTargetVelocity(clientID, rightMotor, -motorSpeed, sim.simx_opmode_blocking)
-        time.sleep(0.1)
+    sim.simxSetJointTargetVelocity(
+            clientID, 
+            leftMotor, 
+            config["motorSpeed"], 
+            sim.simx_opmode_blocking)
+    sim.simxSetJointTargetVelocity(
+            clientID, 
+            rightMotor, 
+            config["motorSpeed"]/2, 
+            sim.simx_opmode_blocking)
+    time.sleep(0.1)
 
     sim.simxFinish(clientID)    
---
+++ runRemoteAPI.bat
@@ -0,0 +1,4 @@
+rem Start to remote API
+
+cd remoteAPI/
+py remoteApiTest.py
